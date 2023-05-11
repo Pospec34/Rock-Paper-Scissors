787050013ceee307438116a6f289d595f3edb2b0
Petr Pospisil <pospisilpetr631@gmail.com>
2023-05-11 11:42:09 +0200

    added while loop

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function playRound(playerSelection, computerSelection){
             return "You win, rock beats scissors"
     } else if
         (playerSelection === "paper" && computerSelection === "rock"){
-            return "You win, paper beaxts rock"
+            return "You win, paper beats rock"
     }   else if
         (playerSelection === "paper" && computerSelection === "scissors"){
             return "You lose, scissors beat paper"
@@ -40,25 +40,40 @@ function playRound(playerSelection, computerSelection){
  }
 
 function game (){
-    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
-    playerSelection = playerSelection.toLowerCase();
-    let result = playRound(playerSelection, getComputerChoice());
-    console.log(result);
-    
-    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
-    playerSelection = playerSelection.toLowerCase();
-    result = playRound(playerSelection, getComputerChoice());
-    console.log(result);
-
+    let result;
+    let w = 0;
+    let l = 0;
+    while (w < 5 && l < 5) {
     playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
     playerSelection = playerSelection.toLowerCase();
     result = playRound(playerSelection, getComputerChoice());
     console.log(result);
-
-    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
-    playerSelection = playerSelection.toLowerCase();
-    result = playRound(playerSelection, getComputerChoice());
-    console.log(result);
+    if (result.indexOf('win') > -1){
+        w++;
+        console.log("Player: " + w);
+        console.log("Computer: " + l);
+    } else if
+        (result.indexOf("lose") > -1){
+        l++;
+        console.log("Player: " + w);
+        console.log("Computer: " + l);
+    }
+    }
+    if (w > l) {
+        console.log("Congratulations! You have won the game!")
+    } else {
+        console.log("Sorry, maybe next time...")
+    }
 }
 
 console.log(game());
+
+
+/*function loop (hodnota){
+let i = parseInt(hodnota);
+while (i < 10000){
+    console.log("Ahoj");
+    i++;
+}}
+
+console.log(loop(0));*/
