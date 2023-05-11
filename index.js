0b7355eb2905650fd06f526441c20c85a8546fa9
Petr Pospisil <pospisilpetr631@gmail.com>
2023-05-11 10:55:50 +0200

    function game added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-playerChoice = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
-playerChoice = playerChoice.toLowerCase();
+
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -7,48 +6,59 @@ function getComputerChoice(){
         return "rock"
     } else if 
         (randomNumber === 2){
-            return "paper"
+        return "paper"
     } else {
         return "scissors"
     } 
-    /*if (randomNumber === 1){
-        return "Rock"
-    }
-    else if (randomNumber === 2)
-    {
-        return "Paper"
-    } else
-        return "Scissors"*/
 }
+let computerSelection = getComputerChoice();
 
-function playRound(playerSelection, aiSelection){
-    if (playerSelection === aiSelection){
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
         return "Draw";
     } else if
-        (playerSelection === "rock" && aiSelection === "paper"){
+        (playerSelection === "rock" && computerSelection === "paper"){
             return "You lose, paper beats rock"
     } else if 
-        (playerSelection === "rock" && aiSelection === "scissors"){
+        (playerSelection === "rock" && computerSelection === "scissors"){
             return "You win, rock beats scissors"
     } else if
-        (playerSelection === "paper" && aiSelection === "rock"){
-            return "You win, paper beats rock"
+        (playerSelection === "paper" && computerSelection === "rock"){
+            return "You win, paper beaxts rock"
     }   else if
-        (playerSelection === "paper" && aiSelection === "scissors"){
+        (playerSelection === "paper" && computerSelection === "scissors"){
             return "You lose, scissors beat paper"
     }   else if 
-        (playerSelection === "scissors" && aiSelection === "rock"){
+        (playerSelection === "scissors" && computerSelection === "rock"){
             return "You lose, rock beats scissors"
         }   else if 
-        (playerSelection === "scissors" && aiSelection === "paper"){
+        (playerSelection === "scissors" && computerSelection === "paper"){
             return "You win, scissors beat paper"
     }   else {
-        return "chyba"
+        return "Error"
     }
  }
- const computerSelection = getComputerChoice();
 
-console.log(playRound(playerChoice, computerSelection))
+function game (){
+    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
+    playerSelection = playerSelection.toLowerCase();
+    let result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+    
+    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
+    playerSelection = playerSelection.toLowerCase();
+    result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
 
+    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
+    playerSelection = playerSelection.toLowerCase();
+    result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
 
-console.log(computerSelection);
+    playerSelection = prompt("Hey, let's try beating PC at Rock, paper, scissors! Choose one.");
+    playerSelection = playerSelection.toLowerCase();
+    result = playRound(playerSelection, getComputerChoice());
+    console.log(result);
+}
+
+console.log(game());
